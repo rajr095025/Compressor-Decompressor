82f6ce151dfc709fc33d623df143837a1f2c1c26
jaya vignesh <rajr095025@gmail.com>
2023-01-05 13:53:49 +0530

    final stages

---
+++ src/main/java/GUI/AppFrame.java
@@ -0,0 +1,80 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package GUI;
+
+import compressor.Decompressor;
+import compressor.compressor;
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author jaivi
+ */
+public class AppFrame extends JFrame implements ActionListener{
+    JButton compressButton;
+    JButton decompressButton;
+    
+    AppFrame(){
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLayout(null);
+        
+        compressButton = new JButton("Select file to compress");
+        compressButton.addActionListener(this);
+        compressButton.setBounds(20,100,200,30);
+        
+        decompressButton = new JButton("select file to decompress");
+        decompressButton.addActionListener(this);
+        decompressButton.setBounds(250,100,200,30);
+        
+        this.add(compressButton);
+        this.add(decompressButton);
+        this.setSize(500,200);
+        this.getContentPane().setBackground(Color.black);
+        this.setVisible(true);  
+    }
+    
+    @Override
+    public void actionPerformed(ActionEvent e){
+        if(e.getSource() == compressButton){
+            JFileChooser filechooser = new JFileChooser();
+            int response = filechooser.showSaveDialog(null);
+            
+            if(response == JFileChooser.APPROVE_OPTION){
+                File file = new File(filechooser.getSelectedFile().getAbsolutePath());
+                //System.out.println(file);
+                try{
+                    compressor.method(file);
+                }
+                catch(Exception excp){
+                    JOptionPane.showMessageDialog(null,excp.toString());
+                }
+            }
+        }
+        if(e.getSource() == decompressButton){
+            JFileChooser filechooser = new JFileChooser();
+            int response = filechooser.showSaveDialog(null);
+            
+            if(response == JFileChooser.APPROVE_OPTION){
+                File file = new File(filechooser.getSelectedFile().getAbsolutePath());
+                //System.out.println(file);
+                try{
+                    Decompressor.method(file);
+                }
+                catch(Exception excp){
+                    JOptionPane.showMessageDialog(null,excp.toString());
+                }
+            }
+        }
+    }
+    
+}
---
+++ src/main/java/compressor/compressor.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package compressor;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.zip.GZIPOutputStream;
+
+/**
+ *
+ * @author jaivi
+ */
+public class compressor {
+    public static void method(File file)throws IOException{
+        String filedirectory = file.getParent();
+        FileInputStream fis = new FileInputStream(file);
+        FileOutputStream fos = new FileOutputStream(filedirectory + "/Compressed1.gz");
+        GZIPOutputStream gzip = new GZIPOutputStream(fos);
+        byte [] buffer = new byte[1024];
+        int len;
+        while((len = fis.read(buffer))!=-1){
+            gzip.write(buffer,0,len);
+        }
+        gzip.close();
+        fis.close();
+        fos.close();
+    }
+    public static void main(String [] args) throws IOException{
+        File file  = new File("E:/rocky.txt");
+        method(file);
+    }
+}
